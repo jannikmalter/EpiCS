1ae1df99757896da60a34d205a65e3d2804174ea
Jannik Malter <jannikmalter@live.de>
2020-02-27 18:06:24 +0000

    working DS18B20

--- DS18B20/DS18B20App/src/tempReader.c
+++ DS18B20/DS18B20App/src/tempReader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h> 
 #include <unistd.h>
 
 #include <dbDefs.h>
@@ -9,14 +10,28 @@
 #include <aSubRecord.h>
 #include <epicsExport.h>
 
+#define BUFSIZE 1024
+
 
 static long mySubProcess(subRecord *precord)
 {
-
-	double val = 42;
-	double * pvPtr;
+    double * pvPtr;
 	pvPtr = &precord->a;
-	*pvPtr = val;
+
+    int input_fd;
+    char buffer[BUFSIZE]; 
+    char* eptr;
+
+    double temperature;
+    
+    input_fd = open("/sys/bus/w1/devices/28-01144e834aaa/w1_slave", O_RDONLY);      
+    read(input_fd, &buffer, BUFSIZE);
+    
+    char* loc = strstr(buffer,"t=");
+    temperature = (double)strtol(loc+2, &eptr, 10) / 1000;
+    
+    close(input_fd);	
+	*pvPtr = temperature;
     return 0;
 }
 
